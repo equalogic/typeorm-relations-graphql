7840c178ae91fbad69540dcdc46d4f796ce95e03
Simon Garner <simon.garner@madscience.co.nz>
2020-03-22 17:27:39 +1300

    Initial project setup

---
+++ .eslintrc.js
@@ -0,0 +1,90 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'prettier',
+  ],
+  // add your custom rules here
+  rules: {
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': [
+      'error',
+      'as-needed',
+      {
+        requireForBlockBody: false,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: { minProperties: 2, consistent: true },
+        ObjectPattern: { minProperties: 5, consistent: true },
+        ImportDeclaration: { consistent: true },
+        ExportDeclaration: { consistent: true },
+      },
+    ],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 0,
+        maxBOF: 0,
+      },
+    ],
+    indent: 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': 'off',
+    '@typescript-eslint/no-parameter-properties': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/consistent-type-assertions': [
+      'error',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow-as-parameter',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      {
+        allowExpressions: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+    {
+      files: ['*.ts'],
+      rules: {
+        // allow TypeScript method signature overloading, see https://github.com/typescript-eslint/typescript-eslint/issues/291
+        'no-dupe-class-members': 'off',
+      },
+    },
+  ],
+};
