fa64db16188a7b3902df2104956e62cc347bf961
mucaho <mucaho@gmail.com>
2023-04-30 07:43:58 +0200

    GraphRelationBuilder: use property checks for prop metadata
    Use property checks to determine the type of the property's metadata, which is more resilient than checking for the metadata's class.

--- src/GraphRelationBuilder.ts
+++ src/GraphRelationBuilder.ts
@@ -73,12 +73,12 @@ export class GraphRelationBuilder {
           this.getEntityMetadata(currentTargetEntity).findEmbeddedWithPropertyPath(propPath);
 
         if (propMetadata != null) {
-          if (propMetadata instanceof RelationMetadata) {
+          if ('inverseEntityMetadata' in propMetadata) {
             currentTargetEntity = propMetadata.inverseEntityMetadata.target;
             nextLevel = currentLevel + 1;
             currentPropertyPath.push(propMetadata.propertyName);
             relationMap.add(currentPropertyPath);
-          } else if (propMetadata instanceof EmbeddedMetadata) {
+          } else if ('propertyPath' in propMetadata) {
             currentPropertyPath.push(propMetadata.propertyPath);
           }
         }
